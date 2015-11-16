ccfee975a46877f1a6a3690571d45736efdd4de8
Dirk Louwers <dirk.louwers@stormlantern.nl>
2015-11-16 01:39:05 +0100

    Improved support for IDE's that use tsconfig.json

--- src/ts/it/DockerTestKitTest.ts
+++ src/ts/it/DockerTestKitTest.ts
@@ -1,10 +1,9 @@
-import chai = require('chai')
+import {assert}  from 'chai'
 import DockerTestKit from '../main/DockerTestKit'
 import Orchestrator from '../main/Orchestrator'
-const assert = chai.assert
 
 describe('DockerTestKit', () => {
   it('should create an Orchestrator', () => {
-      assert.ok(DockerTestKit.createOrchestrator() instanceof Orchestrator)
+    assert.ok(DockerTestKit.createOrchestrator() instanceof Orchestrator)
   })
 })
